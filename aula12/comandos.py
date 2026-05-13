c0ca9e05dc8ef85f42af5884db5ebe99918a12da
JuanCarlos1205 <jacho.juan.carlos@gmail.com>
2026-05-12 21:23:21 -0300

    Aula_12

---
+++ aula12/comandos.py
@@ -0,0 +1,10 @@
+# Ambiente virtual
+# python -m venv venv
+# source ./venv/Scripts/activate
+# Instalar as bibliotecas pandas....
+# deactivate (interrompe o venv)
+# rm -rf venv  (apaga o venv)
+
+# python -m venv venv   (crear ambiente virtual para instalar requiremnts.txt)
+# pip install -r requirements.txt
+
---
+++ aula12/exemplo01.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+
+print('Listas do Python e Series do Pandas')
+
+
