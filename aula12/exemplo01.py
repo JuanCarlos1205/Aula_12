c555d0e5e4b99fd03c557cfe0aae3b41df3097dd
JuanCarlos1205 <jacho.juan.carlos@gmail.com>
2026-05-12 21:50:57 -0300

    Aula 12 exemplo01

--- aula12/exemplo01.py
+++ aula12/exemplo01.py
@@ -1,5 +1,22 @@
 import pandas as pd 
 
 print('Listas do Python e Series do Pandas')
+produtos = ['Notebooks', 'Smartphone', 'Tablet', 'Smartwach', 'Camera']
+quantidade_estoque = [15, 30, 20, 10, 25]
+print(produtos)
+print(quantidade_estoque)
+
+series = pd.Series(produtos)
+print(series)
+
+
+# Indice personalizado
+estoque = pd.Series(quantidade_estoque, index=produtos)
+print(estoque)
+
+
+# type diz de que tipo e o objeto
+#print(type(series))
+#print(type(quantidade_estoque))
 
 
